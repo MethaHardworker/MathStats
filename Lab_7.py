71c99eac3d17d358d19d126a604469659548bd32
MethaHardworker <karasevalexander1999@gmail.com>
2020-04-29 11:19:37 +0300

    Lab7 uniform added

--- Lab_7.py
+++ Lab_7.py
@@ -15,6 +15,15 @@ def binCount(arr, bounds):
     return res
 
 
+def binCountU(arr, bounds):
+    res = [0] * (len(bounds) - 1)
+    for x in arr:
+        for i in range(len(bounds) - 1):
+            if bounds[i] < x < bounds[i + 1]:
+                res[i] += 1
+                break
+    return res
+
 def pCounts(bounds):
     F = [0] * (len(bounds) + 1)
     F[0] = sc.norm.cdf(bounds[0])
@@ -24,15 +33,27 @@ def pCounts(bounds):
     return F
 
 
+def pCountsU(bounds):
+    F = [0] * (len(bounds) - 1)
+    for i in range(1, len(bounds)):
+        F[i - 1] = sc.norm.cdf(bounds[i]) - sc.norm.cdf(bounds[i - 1])
+    return F
+
+
 n = 100
+n1 = 25
 a_s = [-1.5, -0.5, 0, 0.5, 1.5]
+a_s1 = [-2, -1, 0, 1, 2]
 data = sc.norm.rvs(size=n, scale=1, loc=0)
+data1 = sc.uniform.rvs(size=n1, scale=4, loc=-2)
 print(sc.norm.fit(data))
 n_s = binCount(data, a_s)
+n_s1 = binCountU(data1, a_s1)
+p_s1 = pCountsU(a_s1)
 p_s = pCounts(a_s)
 
-print('ps:', p_s)
 
+print('ps1: ', p_s1)
 
 for i in range(len(p_s)):
     print(i + 1, end=' & ')
@@ -47,3 +68,13 @@ for i in range(len(p_s)):
     print(n * p_s[i], end=' & ')
     print(n_s[i] - n * p_s[i], end=' & ')
     print(((n_s[i] - n * p_s[i]) ** 2) / (n * p_s[i]), end=' \\\\ \\hline\n')
+
+for i in range(len(p_s1)):
+    print(i + 1, end=' & ')
+    print('$[', a_s1[i], a_s1[i + 1], ']$', end=' & ')
+    print(n_s1[i], end=' & ')
+    print(p_s1[i], end=' & ')
+    print(n1 * p_s1[i], end=' & ')
+    print(n_s1[i] - n1 * p_s1[i], end=' & ')
+    print(((n_s1[i] - n1 * p_s1[i]) ** 2) / (n1 * p_s1[i]), end=' \\\\ \\hline\n')
+
